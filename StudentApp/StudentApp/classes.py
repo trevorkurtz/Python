19c334899022b3383d86f3ca7f3646cbbb1fd734
Trevor Kurtz <trevorkurtz@outlook.com>
2018-06-27 17:10:38 -0700

    adding classes support

--- StudentApp/StudentApp/classes.py
+++ StudentApp/StudentApp/classes.py
@@ -1,14 +1,32 @@
 students = []
 
 class Student:
+
+    school_name = "Test Univerity"
+
     def __init__(self, name, student_id = 332):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
 
 
-mark = Student("Mark")
+class HighSchoolStudent(Student):
+    school_name = "Test High School"
 
-print(mark)
+    def get_school_name(self):
+        return "This is a high school student " + super().get_school_name()
+
+
+james = HighSchoolStudent("james")
+print(james.get_name_capitalize())
+print(james.get_school_name())
+
